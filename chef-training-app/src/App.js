d99a8684989ec406d4d452a244e098aa44883cd9
sanamcha <mhr.sanam@gmail.com>
2022-04-02 06:49:20 -0700

    initial commit

---
+++ chef-training-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Best Food Collection App....
+      </h1>
+    </div>
+  );
+}
+
+export default App;
