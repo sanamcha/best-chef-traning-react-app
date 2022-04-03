982211589055143c69c322cf72ba375ddd131079
sanamcha <mhr.sanam@gmail.com>
2022-04-02 19:54:48 -0700

    modified App

--- chef-training-app/src/App.js
+++ chef-training-app/src/App.js
@@ -1,63 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import NavBar from "./NavBar";
+// import NavBar from "./NavBar";
 import { Route, Switch } from "react-router-dom";
-import MealApi from "./Api";
-import Homepage from "./components/Homepage";
+import Homepage from "./Routes/Homepage";
+import MealId from "./Routes/MealId";
+import Search from "./Routes/Search";
 import './App.css';
 
 
 
-function App() {
-  const[isLoading, setIsLoading] = useState(true);
-  const [mealsId, setMealsId] = useState([]);
-  const[meals, setMeals] = useState([])
-  const[ingredients, setIngredients] = useState([]);
-
-  useEffect(() => {
-    async function getLatestMeal(){
-      let mealsId = await MealApi.getMealDetails();
-      let meals = await MealApi.getRandomMeals();
-      let ingredients = await MealApi.getIngredient();
-      setMealsId(mealsId);
-      setMeals(meals);
-      setIngredients(ingredients);
-      setIsLoading(false);
-    }
-    getLatestMeal();
-  }, []);
-
-  if( isLoading ) {
-    return <p> Loading.......</p>
-  }
-
-  return (
-    <div className="App">
+const App = () => {
+    return (
+ 
       <BrowserRouter>
-        <NavBar />
-        <main>
-          <Switch>
-            <Route exact path="/">
-              <Homepage />
-            </Route>
-            <Route exact path="/meal/:id">
-              <MealDetail />
-            </Route>
-            <Route path="/search">
-              <Search />
-            </Route>
-            
-            <Route exact path="/ingredients/:id">
-              <Ingredients />
-            </Route>  
-          
-            <Route>
-              <p>Sorry... I can't seem to find what you want.</p>
-            </Route>
-          </Switch>
-        </main>
-      </BrowserRouter>
-    </div>
+
+        {/* <NavBar /> */}
+      <main>
+        <Switch>
+          <Route exact path="/"><Homepage /></Route>
+          <Route exact path="/meal/:id"><MealId /></Route>
+          <Route path="/search"><Search /></Route>
+          {/* <Route exact path="/ingredients/:id"><Ingredients /></Route> */}
+   
+        </Switch>
+      </main>
+    </BrowserRouter>
+
   );
 }
 
