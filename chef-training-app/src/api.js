c69ff7b144df704b24cde71525f44a4e6f6f708f
sanamcha <mhr.sanam@gmail.com>
2022-04-02 19:58:47 -0700

    fix api url

--- chef-training-app/src/api.js
+++ chef-training-app/src/api.js
@@ -1,17 +1,77 @@
-const BASE_API = {
-    mealDetails : "www.themealdb.com/api/json/v1/1/lookup.php?i=",
-    randomMeal : "www.themealdb.com/api/json/v1/1/random.php",
-    mealCategories : "www.themealdb.com/api/json/v1/1/categories.php",
-    searchByName : "www.themealdb.com/api/json/v1/1/search.php?s=",
-    mealAreas : "www.themealdb.com/api/json/v1/1/list.php?a=list",
-    ingredients : "www.themealdb.com/api/json/v1/1/list.php?i=list",
-    byCategory : "www.themealdb.com/api/json/v1/1/filter.php?c=",
-    byArea : "www.themealdb.com/api/json/v1/1/filter.php?a=",
-    filterByIngredient : "www.themealdb.com/api/json/v1/1/filter.php?i=",
-    filterByCatagory : "www.themealdb.com/api/json/v1/1/filter.php?c=",
-    image : "www.themealdb.com/images/ingredients"
+import axios from 'axios';
 
+const BASE_API = {
+    mealDetails : "https://www.themealdb.com/api/json/v1/1/lookup.php?i=",
+    randomMeal : "https://www.themealdb.com/api/json/v1/1/random.php",
+    mealCategories : "https://www.themealdb.com/api/json/v1/1/categories.php",
+    searchByName : "https://www.themealdb.com/api/json/v1/1/search.php?s=",
+    mealAreas : "https://www.themealdb.com/api/json/v1/1/list.php?a=list",
+    ingredients : "https://www.themealdb.com/api/json/v1/1/list.php?i=list",
+    byCategory : "https://www.themealdb.com/api/json/v1/1/filter.php?c=",
+    byArea : "https://www.themealdb.com/api/json/v1/1/filter.php?a=",
+    filterByIngredient : "https://www.themealdb.com/api/json/v1/1/filter.php?i=",
+    filterByCatagory : "https://www.themealdb.com/api/json/v1/1/filter.php?c=",
+    image : "https://www.themealdb.com/images/ingredients"
 }
+
+
+
+    const getMealDetails =  async (id) => {
+        const result = await fetch(`${BASE_API.mealDetails}${id}`);
+        const data = await result.json();
+        return data;
+      };
+
+    async function getSearchByName(meal) {
+        const result = await fetch(`${BASE_API.searchByName}${meal}`);
+        const data = await result.json();
+        return data.meals;
+    }  
+
+    // async function getRandomMeals(){
+    //     const result = await fetch(BASE_API.randomMeal);
+    //     let data = await result.json();
+    //     return data;
+    // }
+
+    const getRandomMeals = async () => {
+        let randMeals = [];
+        while(randMeals.length !== 10){
+            let result = await fetch(BASE_API.randomMeal);
+            let data = await result.json();
+            let isDup = !!randMeals.find((meal) => JSON.stringify(meal) === JSON.stringify(data.meals[0]));
+            if(!isDup) {
+                randMeals.push(data.meals[0]);
+            }
+        }
+        return randMeals;  
+    }
+
+
+    async function getAreas(){
+        const result = await fetch(BASE_API.mealAreas);
+        const data = result.json();
+        return data.meals;
+    }
+
+    const getIngredientImage = (ingredient) => {
+        const imgResult = `${BASE_API.image}/${ingredient}.png`;
+        return imgResult;
+    };
+
+    async function getIngredient() {
+        const result = await fetch(`${BASE_API.ingredients}`);
+        const data = await result.json();
+        
+        return data.meals;
+    }
+    async function getFilterByIngredient(ingredientName) {
+        const result = await fetch(`${BASE_API.filterByIngredient}${ingredientName}`);
+        const data = await result.json();
+
+        return data.meals;
+    };
+
 
 const mealData = [
     {
@@ -77,3 +137,15 @@ const mealData = [
         strTags: "MainMeal",
         },
 ]
+export { 
+    mealData,
+    getMealDetails,
+    getSearchByName,
+    getRandomMeals,
+    getAreas,
+    getIngredientImage,
+    getIngredient,
+    getFilterByIngredient
+ } 
+
+export default BASE_API;
