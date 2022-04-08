ca6bfe830095d753242cf840bc78484b795d06c4
sanamcha <mhr.sanam@gmail.com>
2022-04-08 15:47:08 -0700

    add some extra links

---
+++ chef-training-app/src/common/MoreLinks.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const MoreLinks = () => {
+
+    return (
+        <div>
+
+            <Link to="/ingredients/0">Chicken</Link>
+            <Link to="/ingredients/1">Salmon</Link>
+            <Link to="/ingredients/2">Beef</Link>
+            <Link to="/ingredients/3">Pork</Link>
+            <Link to="/ingredients/4">Avacado</Link>
+            <Link to="/ingredients/110">Egg Yolks</Link>
+            <Link to="/ingredients/6">Asparagus</Link>
+            <Link to="/ingredients/7">Aubergine</Link>
+            <Link to="/ingredients/8">Tomatos</Link>
+            <Link to="/ingredients/9">Bacon</Link>
+            <Link to="/ingredients/10">Balsamic Vinegar</Link>
+            <Link to="/ingredients/11">Baking Powder</Link>
+            <Link to="/ingredients/79">Coconut Cream</Link>
+            <Link to="/ingredients/31">Broccoli</Link>
+            <Link to="/ingredients/50">Charlotte Potatoes</Link>
+            <Link to="/ingredients/60">Chicken Thighs</Link>
+            <Link to="/ingredients/70">Chopped Tomatoes</Link>
+            <Link to="/ingredients/80">Coconut Milk</Link>
+            <Link to="/ingredients/90">Creme Fraiche</Link>
+            <Link to="/ingredients/100">Diced Tomatoes</Link>
+            <Link to="/ingredients/200">Monterey Jack Cheese</Link>
+        </div>
+    )
+}
+
+export default MoreLinks;
