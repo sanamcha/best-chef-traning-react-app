f3f7eee4e86573dea33d6ead1db9997686478654
sanamcha <mhr.sanam@gmail.com>
2022-04-08 15:53:54 -0700

    added meal-data for homepage

---
+++ chef-training-app/src/db.js
@@ -0,0 +1,150 @@
+
+
+const MealData = [
+    
+               {
+                   "idMeal": "52767",
+                   "strMeal": "Bakewell tart",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Dessert",
+                   "strArea": "British",
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/wyrqqq1468233628.jpg",
+                   "strTags": "Tart,Baking,Alcoholic",
+                   "strYoutube": "https://www.youtube.com/watch?v=1ahpSTf_Pvk"
+               
+               },
+               {
+                   "idMeal": "52792",
+                   "strMeal": "Bread and Butter Pudding",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Dessert",
+                   "strArea": "British",
+               
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/xqwwpy1483908697.jpg",
+                   "strTags": "Pudding,Brunch",
+                   "strYoutube": "https://www.youtube.com/watch?v=Vz5W1-BmOE4"
+               
+               },
+               {
+                   "idMeal": "52803",
+                   "strMeal": "Beef Wellington",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Beef",
+                   "strArea": "British",
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/vvpprx1487325699.jpg",
+                   "strTags": "Meat,Stew",
+                   "strYoutube": "https://www.youtube.com/watch?v=FS8u1RBdf6I"
+               },
+               {
+                   "idMeal": "52807",
+                   "strMeal": "Baingan Bharta",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Vegetarian",
+                   "strArea": "Indian",
+               
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/urtpqw1487341253.jpg",
+                   "strTags": "Spicy,Bun,Calorific",
+                   "strYoutube": "https://www.youtube.com/watch?v=-84Zz2EP4h4"
+               
+               },
+               {
+                   "idMeal": "52812",
+                   "strMeal": "Beef Brisket Pot Roast",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Beef",
+                   "strArea": "American",
+               
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/ursuup1487348423.jpg",
+                   "strTags": "Meat",
+                   "strYoutube": "https://www.youtube.com/watch?v=gh48wM6bPWQ"
+
+               
+               },
+               {
+                   "idMeal": "52824",
+                   "strMeal": "Beef Sunday Roast",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Beef",
+                   "strArea": "British",
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/ssrrrs1503664277.jpg",
+                   "strTags": "MainMeal",
+                   "strYoutube": "https://www.youtube.com/watch?v=2l3-dBdNehY"
+
+               },
+               {
+                   "idMeal": "52795",
+                   "strMeal": "Chicken Handi",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Chicken",
+                   "strArea": "Indian",
+       
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg",
+                   "strTags": null,
+                   "strYoutube": "https://www.youtube.com/watch?v=IO0issT0Rmc"
+       
+               },
+               {
+                   "idMeal": "52956",
+                   "strMeal": "Chicken Congee",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Chicken",
+                   "strArea": "Chinese",
+       
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/1529446352.jpg",
+                   "strTags": null,
+                   "strYoutube": "https://www.youtube.com/watch?v=kqEfk801E94"
+       
+       
+               },
+               {
+                   "idMeal": "52831",
+                   "strMeal": "Chicken Karaage",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Chicken",
+                   "strArea": "Japanese",
+       
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/tyywsw1505930373.jpg",
+       
+                   "strYoutube": "https://www.youtube.com/watch?v=XivddFddthc",
+               },
+               
+               {
+                   "idMeal": "52802",
+                   "strMeal": "Fish pie",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Seafood",
+                   "strArea": "British",
+       
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/ysxwuq1487323065.jpg",
+                   "strTags": "Fish,Pie,Breakfast,Baking",
+                   "strYoutube": "https://www.youtube.com/watch?v=2sX4fCgg-UI"
+       
+               },
+               {
+                   "idMeal": "53043",
+                   "strMeal": "Fish fofos",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Seafood",
+                   "strArea": "Portuguese",
+       
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/a15wsa1614349126.jpg",
+                   "strTags": null,
+                   "strYoutube": "https://www.youtube.com/watch?v=YXUZHK2PrV8"
+       
+               },
+               {
+                   "idMeal": "52882",
+                   "strMeal": "Three Fish Pie",
+                   "strDrinkAlternate": null,
+                   "strCategory": "Seafood",
+                   "strArea": "British",
+       
+                   "strMealThumb": "https://www.themealdb.com/images/media/meals/spswqs1511558697.jpg",
+                   "strTags": "Fish,Seafood,Dairy,Pie",
+                   "strYoutube": "https://www.youtube.com/watch?v=Ds1Jb8H5Sg8"
+       
+               }
+       
+           ]
+       
+export default MealData;
