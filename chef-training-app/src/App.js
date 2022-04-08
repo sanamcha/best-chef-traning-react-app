7011bc45803424079e6b0281ed560af9b5019c1f
sanamcha <mhr.sanam@gmail.com>
2022-04-08 15:45:42 -0700

    updated app

--- chef-training-app/src/App.js
+++ chef-training-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-// import NavBar from "./NavBar";
-import { Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Homepage from "./Routes/Homepage";
 import MealId from "./Routes/MealId";
 import Search from "./Routes/Search";
+import IngredientId from "./Routes/IngredientId";
+import RandomMeal from "./Routes/RandomMeal";
+import Categories from "./components/Categories";
 import './App.css';
 
 
@@ -14,13 +16,17 @@ const App = () => {
  
       <BrowserRouter>
 
-        {/* <NavBar /> */}
+        <NavBar />
       <main>
         <Switch>
           <Route exact path="/"><Homepage /></Route>
           <Route exact path="/meal/:id"><MealId /></Route>
-          <Route path="/search"><Search /></Route>
-          {/* <Route exact path="/ingredients/:id"><Ingredients /></Route> */}
+          <Route exact path="/random"><RandomMeal /></Route>
+          <Route exact path="/categories"><Categories /></Route>
+          <Route path="/search/q=:searchFor"><Search /></Route>
+          
+          <Route exact path="/ingredients/:id"><IngredientId /></Route>
+          
    
         </Switch>
       </main>
