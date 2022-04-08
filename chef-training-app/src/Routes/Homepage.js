2e3a5269a74513d2a508bbcbc1cf6feb5604774c
sanamcha <mhr.sanam@gmail.com>
2022-04-08 15:46:22 -0700

    updated homepage

--- chef-training-app/src/Routes/Homepage.js
+++ chef-training-app/src/Routes/Homepage.js
@@ -1,51 +1,25 @@
 import React, { useState, useEffect } from "react";
 import Meals from "../components/Meals";
-import { getRandomMeals } from "../Api";
-import { mealData as latestMeals } from "../Api";
-import { isEmpty } from "../common/helper";
-import LoadingSpinner from "../common/LoadingSpinner";
+import MealData from "../db";
 
+
+import "../App.css";
 
 const Homepage = () => {
-    const [ meals, setMeals ] = useState([]);
-    const [ mealData ] = useState(latestMeals)
-
-
-    useEffect(() => {
-        results();
-    }, []);
-
-    const results = async () => {
-        const meals = await getRandomMeals();
-        console.log(getRandomMeals);
-        setMeals(meals);
-    }
-   
-   
-    // if(!meals) return <LoadingSpinner />
     
     return (
         <div>
-            <h1>Home page..</h1>
             <div>
                 <h4>
-                    Best Meals
+                     The Best Meal Collection
                 </h4>
-                <div>
-                    <Meals meals={ mealData } />
-                </div>
+                
             </div>
-
-            { isEmpty(meals) ? (
-                <LoadingSpinner />
-            ) : (
-                <>
-                <h4>Random Meals:</h4>
-                <div>
-                    <Meals meals={meals} />
-                </div>
-                </>
-            )}
+       
+            <div>
+                <Meals meals={ MealData } />
+            </div>
+          
         </div>
     );
 }
