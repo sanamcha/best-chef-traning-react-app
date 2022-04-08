a2ac75a82230b1b7d57f9c934c6af2ff1f575ac2
sanamcha <mhr.sanam@gmail.com>
2022-04-08 16:30:02 -0700

    modified random meal

---
+++ chef-training-app/src/Routes/RandomMeal.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState}from "react";
+import Meals from "../components/Meals";
+import LoadingSpinner from "../common/LoadingSpinner";
+import {isEmpty} from "../common/Helper.js";
+import { getRandomMeals } from "../Api";
+
+const RandomMeal = () => {
+    const [ meals, setMeals ] = useState([]);
+    useEffect(() => {
+       result();
+    }, []) 
+
+    const result = async () => {
+        const meals = await getRandomMeals();
+        console.log(getRandomMeals());
+        setMeals(meals);
+    }
+        
+    
+    return (
+        <div>
+            <h4 className="randomMeals">Our Popular Meals:</h4>
+            { isEmpty(meals) ? (
+                <LoadingSpinner />
+            ) : (
+            <div>
+                <Meals meals={meals} />
+            </div>
+            )}
+        </div>
+    );
+
+}
+export default RandomMeal;
+
